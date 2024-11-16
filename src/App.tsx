b5f87f26ab6f9ec6cbb25328236ff796e7f42ab7
nyk9 <eijikagimoto@gmail.com>
2024-11-16 10:56:09 +0900

    delete: Countコンポーネントを削除

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Count from "./components/Count";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -26,7 +25,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <Count />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
